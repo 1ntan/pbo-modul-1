1cc7f26a11cf4909da71e76d9c5719bd9b7c5c75
1ntan <intandrahmadhani@gmail.com>
2024-02-27 09:17:22 +0700

    Menambahkan boolean

--- src/main/java/org/tugas/tugas_1.java
+++ src/main/java/org/tugas/tugas_1.java
@@ -15,42 +15,46 @@ public class tugas_1 {
         String passwordUser;
         String option;
         Scanner objInput = new Scanner(System.in);
+        boolean ulang = true;
 
-        System.out.println("Library System");
-        System.out.println("1. Login as Student");
-        System.out.println("2. Login as Admin");
-        System.out.println("3. Exit");
+        while(ulang) {
+            System.out.println("Library System");
+            System.out.println("1. Login as Student");
+            System.out.println("2. Login as Admin");
+            System.out.println("3. Exit");
 
-        System.out.print("Choose option (1-3): ");
-        option = objInput.nextLine();
+            System.out.print("Choose option (1-3): ");
+            option = objInput.nextLine();
 
-        switch (option) {
-            case "1":
-                System.out.print("Enter your NIM: ");
-                nimUser = objInput.nextLine();
-                if (Arrays.asList(nimData).contains(nimUser)) {
-                    System.out.println("Succesful login as Student");
-                } else {
-                    System.out.println("User Not Found");
-                }
-                break;
-            case "2":
-                System.out.print("Enter your username (admin): ");
-                usernameUser = objInput.nextLine();
-                System.out.print("Enter your password (admin): ");
-                passwordUser = objInput.nextLine();
-                if (usernameUser.equals(usernameData) && passwordUser.equals(passwordData)){
-                    System.out.println("Succesful login as Admin");
-                } else {
-                    System.out.println("Admin User Not Found");
-                }
-                break;
-            case "3":
-                System.out.println("adios");
-                break;
-            default:
-                System.out.println("Invalid input");
-                break;
+            switch (option) {
+                case "1":
+                    System.out.print("Enter your NIM: ");
+                    nimUser = objInput.nextLine();
+                    if (Arrays.asList(nimData).contains(nimUser)) {
+                        System.out.println("Succesful login as Student");
+                    } else {
+                        System.out.println("User Not Found");
+                    }
+                    break;
+                case "2":
+                    System.out.print("Enter your username (admin): ");
+                    usernameUser = objInput.nextLine();
+                    System.out.print("Enter your password (admin): ");
+                    passwordUser = objInput.nextLine();
+                    if (usernameUser.equals(usernameData) && passwordUser.equals(passwordData)) {
+                        System.out.println("Succesful login as Admin");
+                    } else {
+                        System.out.println("Admin User Not Found");
+                    }
+                    break;
+                case "3":
+                    System.out.println("adios");
+                    ulang = false;
+                    break;
+                default:
+                    System.out.println("Invalid input");
+                    break;
+            }
         }
     }
 }
